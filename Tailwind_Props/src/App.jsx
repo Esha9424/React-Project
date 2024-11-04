0379846a11e4eee8f1850b43aa2b59271edea49d
Esha <esha2103rita@gmail.com>
2024-11-04 20:43:36 +0530

    pasing obj and array from component to component

--- Tailwind_Props/src/App.jsx
+++ Tailwind_Props/src/App.jsx
@@ -13,11 +13,12 @@ function App() {
 
   }
 
+  let myar=[1,2,3]
   return (
     <>
      <h1  className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
      <Card username="Chai or code" sameobj= {myarr} />
-     <Card username="Chai or code"/>
+     <Card username="Chai or code" Samearr={myar}/>
      
    </>
   )
